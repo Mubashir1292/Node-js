ebde054b810a882a1014332a4701a77346656393
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-05-23 14:37:59 +0500

    basic server setup

--- task-manager/03-task-manager/starter/app.js
+++ task-manager/03-task-manager/starter/app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+//! setting-up the server where the server will listen at the port of 3000
+const express = require("express");
+const app = express();
+
+//* routes setting and handling the new pages
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+const port = 3001;
+app.listen(port, console.log("app"));
