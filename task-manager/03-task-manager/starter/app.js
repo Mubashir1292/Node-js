7f3f3ee91efba9c37046990772747d14d6b6e4d6
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-05-23 15:16:28 +0500

    done the server setupp.....

--- task-manager/03-task-manager/starter/app.js
+++ task-manager/03-task-manager/starter/app.js
@@ -1,7 +1,15 @@
 // * setting the server up and done the changes at my best...
 const express = require("express");
 const app = express();
+const giveMeBackNumbersList = () => {
+  let numbers = [];
+  for (let i = 0; i < 100; i++) {
+    let randomNumber = Math.floor(Math.random() * 99);
+    numbers[i] = randomNumber;
+  }
+  return numbers;
+};
 app.listen(3000, console.log("server gone started"));
 app.get("/hello", (req, res) => {
-  res.send("Management App");
+  res.send("Management App \n" + giveMeBackNumbersList());
 });
