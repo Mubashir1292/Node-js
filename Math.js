4d73bc88f428bbb83332f375550f25c61940593a
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-05-24 14:25:57 +0500

    server setting up

--- Math.js
+++ Math.js
@@ -1,1 +1,10 @@
 exports.giveMeRandomNumber = () => Math.floor(Math.random() * 99);
+
+// create a function and just have to export it from
+const randomFunction = () => {
+  return Math.floor(Math.random() * 99);
+};
+//* export the function by using the following line:
+//? module.exports = { randomFunction };
+//* You can export the function by writing :
+exports.randomFunction = () => Math.floor(Math.random() * 99);
