eafe38dbe8f3d421d22d8f222ecbd7fc94086e11
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-05-24 17:13:54 +0500

    Read and Writing files in node.js

--- 03FileWrite/index.js
+++ 03FileWrite/index.js
@@ -1,15 +1,30 @@
 // * writing the file using the fs.write
 
+//? checking the promises
+
 const fs = require("fs");
 const path = require("path");
+const dataToWrite = "Test Writing on to the File";
 
-const data = "This is a Text File...";
+fs.writeFile(
+  path.join(__dirname, "", "testWriting.txt"),
+  dataToWrite,
+  (err) => {
+    if (err) console.log(err);
+  },
+  console.log("write complete"),
 
-fs.writeFile(path.join(__dirname, "", "reply.txt"), data, (err) => {
-  if (err) console.log(err);
-  console.log("Write Complete");
-});
+  fs.rename(
+    path.join(__dirname, "", "reply.txt"),
+    path.join(__dirname, "", "Testing.txt"),
+    (err) => {
+      if (err) console.log(err);
+      console.log("Rename Completed");
+    }
+  )
+);
+
+// //? checking whether the error occured
 process.on("uncaughtException", (err) => {
-  console.log(`There was an Exception : ${err}`);
-  process.exit;
+  console.log(`There was an Exception about ${err}`);
 });
