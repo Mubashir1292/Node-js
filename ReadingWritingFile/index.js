3104c1aadafc6d0b057176e8fb52ddba4e274d39
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-07-24 13:25:22 +0500

    Reading and Writing files Asynchronously in node.js

--- ReadingWritingFile/index.js
+++ ReadingWritingFile/index.js
@@ -1,8 +1,14 @@
 /** ******   Reading and Writing file   ***/
 //console.log("Read and Write files");
 const fs = require("fs");
-const previousData = fs.readFileSync("./NewFiles/lorem.txt", "utf-8");
-const content = `${previousData}\nNew Data is Written to new File ${
-  new Date().toISOString().split("T")[0]
-}`;
-fs.writeFileSync("./NewFiles/lorem.txt", content, "utf-8");
+// const content = `\nNew Data is Written to new File ${
+//   new Date().toISOString().split("T")[0]
+// }`;
+fs.readFile("./NewFiles/lorem.txt", "utf-8", (err, data) => {
+  fs.readFile(`./NewFiles/${data}.txt`, "utf-8", (err1, data1) => {
+    fs.readFile(`./NewFiles/${data1}.txt`, "utf-8", (err2, data2) => {
+      console.log(data2);
+    });
+  });
+});
+console.log("Reading File");
