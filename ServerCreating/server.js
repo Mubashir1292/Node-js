554fb3e3a641a6f982465716503450fc56273f64
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-07-26 00:42:16 +0500

    Routing with Node JS

--- ServerCreating/server.js
+++ ServerCreating/server.js
@@ -1,11 +1,21 @@
 // * Creating Server */
 const http = require("http");
-const fs = require("fs");
-const data = fs.readFileSync("./index.html", "utf-8");
+// const fs = require("fs");
+//const data = fs.readFileSync("./index.html", "utf-8");
 // creating the server
 const server = http.createServer((request, response) => {
-  response.end(data);
-  console.log("New Request receivend");
+  //! Doing Routing by using the Node JS
+  const path = request.url;
+  if (path === "/" || path.toLocaleLowerCase() === "/home") {
+    response.end("You are at the home page");
+  } else if (path.toLocaleLowerCase() === "/about") {
+    response.end("You are at the About Page");
+  } else {
+    response.end("404 Error: Page Not Founded");
+  }
+
+  // response.end(data);
+  // console.log("New Request receivend");
 });
 server.listen(8000, "127.0.0.1", () => {
   console.log("Server Started");
