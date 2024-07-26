c08fc51410e62ffe207d53f2018d1f011921a659
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-07-26 09:11:12 +0500

    Routing On Different Things

--- ServerCreating/server.js
+++ ServerCreating/server.js
@@ -1,17 +1,19 @@
 // * Creating Server */
 const http = require("http");
-// const fs = require("fs");
-//const data = fs.readFileSync("./index.html", "utf-8");
+const fs = require("fs");
+const data = fs.readFileSync("./index.html", "utf-8");
 // creating the server
 const server = http.createServer((request, response) => {
   //! Doing Routing by using the Node JS
   const path = request.url;
   if (path === "/" || path.toLocaleLowerCase() === "/home") {
-    response.end("You are at the home page");
+    response.end(data.replace("{{%CONTENT%}}", "You are at Home Page"));
+  } else if (path.toLocaleLowerCase() === "/contact") {
+    response.end(data.replace("{{%CONTENT%}}", "You are at Contact Page"));
   } else if (path.toLocaleLowerCase() === "/about") {
-    response.end("You are at the About Page");
+    response.end(data.replace("{{%CONTENT%}}", "You are at About Page"));
   } else {
-    response.end("404 Error: Page Not Founded");
+    response.end(data.replace("{{%CONTENT%}}", "Error 404: Page Not Founded"));
   }
 
   // response.end(data);
