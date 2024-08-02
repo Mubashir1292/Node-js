e2c20307362251e01c34dad741f58c4033ee5533
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-08-02 17:31:09 +0500

    Streams with chunks of data

--- Streaming/server/server.js
+++ Streaming/server/server.js
@@ -4,14 +4,26 @@ const http = require("http");
 const server = http.createServer();
 server.on("request", (req, res) => {
   console.log("New Request Founded");
-  if (req.url === "/" || req.url.toLocaleLowerCase() === "/home") {
-    fs.readFile("./sample.txt", "utf-8", (err, data) => {
-      if (err) {
-        res.end("Something went wronge");
-        return;
-      }
-      res.end(data);
-      console.log("Reading data");
+  // if (req.url === "/" || req.url.toLocaleLowerCase() === "/home") {
+  //   fs.readFile("./sample.txt", "utf-8", (err, data) => {
+  //     if (err) {
+  //       res.end("Something went wronge");
+  //       return;
+  //     }
+  //     res.end(data);
+  //     console.log("Reading data");
+  //   });
+  // }
+  if (req.url === "/") {
+    let rs = fs.createReadStream("./sample.txt");
+    rs.on("data", (chunk) => {
+      res.write(chunk);
+    });
+    rs.on("end", () => {
+      res.end();
+    });
+    rs.on("error", (error) => {
+      res.end(error.message);
     });
   }
 });
