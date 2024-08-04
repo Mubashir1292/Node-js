a967f7c567207141789c1f078596662c9d7f922f
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-08-04 14:18:07 +0500

    Practice of Event Loop

--- nodePackages/server.js
+++ nodePackages/server.js
@@ -11,4 +11,28 @@ server.on("request", (req, res) => {
 server.listen(8000, "127.0.0.1", () => {
   console.log("Server is on");
 });
-console.log("nodemon is just started");
+// console.log("nodemon is just started");
+// console.log("checking the nodemon is working or not...");
+
+//! nodemon and other things as well
+console.log("Program is started");
+
+//! Stored in second phase the IO task
+fs.readFile("./sample.txt", "utf-8", (err, data) => {
+  if (!err) {
+    console.log("File read done");
+  }
+  //! stored in  first phase of the Event Loop
+  setTimeout(() => {
+    console.log("Timeout Function is Excuted");
+  }, 0);
+  //! Stored in Third phase of event loop Immediate phase
+  setImmediate(() => {
+    console.log("Set Immediate function is called");
+  });
+  process.nextTick(() => {
+    console.log("Next Tick Process");
+  });
+});
+
+console.log("Program is Ended");
