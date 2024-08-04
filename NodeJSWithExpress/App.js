d64d48f3b0fdca311d228997faa67d8db080711c
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-08-04 15:25:34 +0500

    Express JS..GET & Post

--- NodeJSWithExpress/App.js
+++ NodeJSWithExpress/App.js
@@ -1,2 +1,19 @@
 //import package
-let express = require("express");
+const express = require("express");
+let app = express();
+
+//Route = HTTP Method + URL
+app.get("/", (req, res) => {
+  //   res.status(200).send(`<h4>you are at the home place</h4>`);
+  res.status(200).json({ message: "Hello World", status: 200 });
+});
+app.post("/", () => {});
+app.get("/404", (req, res) => {
+  res.status(404).send("Error 404:Page not founded");
+});
+
+//create a server
+const port = 3000;
+app.listen(port, () => {
+  console.log("Server On hai...");
+});
